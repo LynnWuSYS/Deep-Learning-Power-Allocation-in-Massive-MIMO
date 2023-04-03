5f172b000134e3feae3e4704ca9941e6f6bc6565
Lynn Wu <Lynn.Wu@intersystems.com>
2023-04-03 16:04:57 -0400

    test

--- functionComputeSE_DL_poweralloc.m
+++ functionComputeSE_DL_poweralloc.m
@@ -7,7 +7,6 @@ function SE = functionComputeSE_DL_poweralloc(rho,signal,interference,prelogFact
 %               power allocated to UE k in cell j
 %signal       = K x L matrix where element (k,j,n) is a_jk in (7.2)
 %interference = K x L x K x L matrix where (l,i,jk,n) is b_lijk in (7.3)
-%prelogFactor = Prelog factor
 %
 %OUTPUT:
 %SE = K x L matrix where element (k,j) is the downlink SE of UE k in cell j
